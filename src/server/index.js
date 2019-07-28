c8316c237e86103b304f0b477fb74373661ad117
archiewald <artur.kozubek1@gmail.com>
2019-07-28 11:13:49 +0200

    init ws server

--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+const ws = require('ws');
+const dotenv = require('dotenv');
+const path = require('path');
+const process = require('process');
+
+dotenv.config({
+    path: path.join(__dirname, 'config', 'app.env')
+});
+
+const port = Number(process.env.PORT);
+const settings = { port };
+
+const server = new ws.Server(settings);
